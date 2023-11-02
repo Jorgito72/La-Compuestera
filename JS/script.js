dd7d9c7b43225f33475e8b373cfd89e9843a8749
Jorge Pablo <pablojorgel01@gmail.com>
2023-11-02 16:06:09 -0300

    se agrego script js

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+const montoInicial= document.getElementById("imputMontoInicial");
+const tasaPorPeriodo= document.getElementById("imputTasaPeriodo");
+const cantidadPeriodos= document.getElementById("imputCantPeriodos");
+let interesesObtenidos = 0;
+let montoFinal = 0;
