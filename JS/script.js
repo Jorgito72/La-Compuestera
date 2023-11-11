8b15e95fee4b03a275ca8dc3b2b0d0c988b773bd
Jorge Pablo <pablojorgel01@gmail.com>
2023-11-11 01:25:45 -0300

    funcionando

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,14 @@
-const montoInicial= document.getElementById("imputMontoInicial");
+const montoInicial= document.getElementById("capital");
 const tasaPorPeriodo= document.getElementById("imputTasaPeriodo");
-const cantidadPeriodos= document.getElementById("imputCantPeriodos");
+const periodos= document.getElementById("imputCantPeriodos");
 let interesesObtenidos = 0;
 let montoFinal = 0;
+
+function calcularInteresCompuesto() {
+var tasaPorcentaje = parseFloat(tasaPorPeriodo.value) / 100;
+ var resultado = parseFloat(montoInicial.value) * Math.pow((1 + tasaPorcentaje), parseFloat(periodos.value));
+ var interesCompuesto = resultado - montoInicial.value;
+  // Mostrar el resultado
+  document.getElementById('montoFinal').innerHTML = 'El resultado del interés compuesto es: ' + resultado.toFixed(2);
+  document.getElementById('intereses').innerHTML = 'El interés obtenido es: ' + interesCompuesto.toFixed(2);
+}
