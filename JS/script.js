5bc301dae0bea1aec3cb0acfcb2fa0745f8f39da
Jorge Pablo <pablojorgel01@gmail.com>
2023-11-25 15:01:32 -0300

    agregando estilos

--- JS/script.js
+++ JS/script.js
@@ -8,7 +8,12 @@ function calcularInteresCompuesto() {
 var tasaPorcentaje = parseFloat(tasaPorPeriodo.value) / 100;
  var resultado = parseFloat(montoInicial.value) * Math.pow((1 + tasaPorcentaje), parseFloat(periodos.value));
  var interesCompuesto = resultado - montoInicial.value;
+ 
+  // Aplicar estilos a las variables antes de mostrarlas
+  var resultadoStyled = `<span style="font-weight: bold; text-decoration: underline;">${resultado.toFixed(2)}</span>`;
+  var interesCompuestoStyled = `<span style="font-weight: bold; text-decoration: underline;">${interesCompuesto.toFixed(2)}</span>`;
+
   // Mostrar el resultado
-  document.getElementById('montoFinal').innerHTML = 'El resultado del interés compuesto es: ' + resultado.toFixed(2);
-  document.getElementById('intereses').innerHTML = 'El interés obtenido es: ' + interesCompuesto.toFixed(2);
+  document.getElementById('montoFinal').innerHTML = 'El resultado del interés compuesto es: ' + resultadoStyled;
+  document.getElementById('intereses').innerHTML = 'El interés obtenido es: ' + interesCompuestoStyled;
 }
